6fa20a9c53de11411c6a9d4cdcaad5ec1198854c
yuvaraj2016 <juvaraj2016@gmail.com>
2021-01-29 14:41:22 +0530

    updates

--- resources/views/create_properties.blade.php
+++ resources/views/create_properties.blade.php
@@ -157,21 +157,24 @@
                                 @csrf
                                 
                                 <div class="form-group row ">
-                                <div class="col-sm-4">
-                                                                <label class="col-form-label text-md-right " >Vendor Name</label>
-
+                                
                                                                 @if(collect(session('roles'))->contains('Vendor'))  
+                                                               
+    
                                                                 <input type="hidden" name="name" value="{{ session('user_id') }}" class="summernote-simple form-control" required>
                                              
                                                                 @else
-                                                                <select  class="js-example-basic-single col-sm-12" name="vendor_id" id=""  required class="form-control selectric" required>
-                                                                    <option value="" selected disabled>Select</option>
-                                                                    @foreach($vendors as $vendorss)
-                                                                        <option value="{{ $vendorss['user_id'] }}" {{ (old("user_id") == $vendorss['id'] ? "selected":"") }}>{{ $vendorss['name'] }}</option>
-                                                                    @endforeach
-                                                                </select>
-                                                                @endif
+                                                                <div class="col-sm-4">
+                                                                    <label class="col-form-label text-md-right " >Vendor Name</label>
+                                                                    <select  class="js-example-basic-single col-sm-12" name="vendor_id" id=""  required class="form-control selectric" required>
+                                                                        <option value="" selected disabled>Select</option>
+                                                                        @foreach($vendors as $vendorss)
+                                                                            <option value="{{ $vendorss['user_id'] }}" {{ (old("user_id") == $vendorss['id'] ? "selected":"") }}>{{ $vendorss['name'] }}</option>
+                                                                        @endforeach
+                                                                    </select>
                                                                 </div>
+                                                                @endif
+                                                              
                                                                 <div class="col-sm-4">
                                                                 <label class="col-form-label text-md-right" >Properties Name ( Min Character:5 )</label>
                                                                 <input name="name" value="{{ old('name') }}" minlength="5" maxlength="500" class="summernote-simple form-control" required>
