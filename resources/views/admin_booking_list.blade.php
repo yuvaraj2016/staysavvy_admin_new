adb00cb1d9d6917bf29829683019cff6162d8eb7
satha091 <sathasivam@hridhamtech.com>
2021-05-03 12:36:49 +0530

    admin booking

--- resources/views/admin_booking_list.blade.php
+++ resources/views/admin_booking_list.blade.php
@@ -469,16 +469,17 @@
                                commissionAmt = commissionItem.commission_amount;
                            }
                        });
-                       
+                       const view = `adminbookings/${item.id}`;
+                       const edit = `adminbookings/${item.id}/edit`;
                        $('#tableBody').append(
                        '<tr><td>'+
                        '<div class="d-flex">'+
                                     '<ul class="list-group list-inline ml-1">'+
                                        '+<li class="list-group-item border1">'+
-                                          '<a href="" class=" d-inline font1 " id="alert1" data-toggle="tooltip" data-placement="top" title="View"><i class="fa fa-eye"></i></a>'+
+                                          '<a href="'+view+'" class=" d-inline font1 " id="alert1" data-toggle="tooltip" data-placement="top" title="View"><i class="fa fa-eye"></i></a>'+
                                        '</li>'+
                                        '<li class="list-group-item border1">'+
-                                          '<a href=""  class=" d-inline font1 edit-confirmation" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></a>'+
+                                        '<a href="'+edit+'"  class="d-inline font1 edit-confirmation" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></a>'+
                                        '</li>'+
                                        '<li class="list-group-item border1">'+
                                           '<form id="" method="POST" action="">'+
@@ -551,16 +552,17 @@
                                commissionAmt = commissionItem.commission_amount;
                            }
                        });
-                       
+                       const view = `adminbookings/${item.id}`;
+                       const edit = `adminbookings/${item.id}/edit`; 
                        $('#tableBody').append(
                        '<tr><td>'+
                        '<div class="d-flex">'+
                                     '<ul class="list-group list-inline ml-1">'+
                                        '+<li class="list-group-item border1">'+
-                                          '<a href="" class=" d-inline font1 " id="alert1" data-toggle="tooltip" data-placement="top" title="View"><i class="fa fa-eye"></i></a>'+
+                                          '<a href="'+view+'" class=" d-inline font1 " id="alert1" data-toggle="tooltip" data-placement="top" title="View"><i class="fa fa-eye"></i></a>'+
                                        '</li>'+
                                        '<li class="list-group-item border1">'+
-                                          '<a href=""  class=" d-inline font1 edit-confirmation" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></a>'+
+                                          '<a href="'+edit+'"  class=" d-inline font1 edit-confirmation" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></a>'+
                                        '</li>'+
                                        '<li class="list-group-item border1">'+
                                           '<form id="" method="POST" action="">'+
