b7149ebbeed7f0238700384cc28c99a0a6c5d340
satha091 <sathasivam@hridhamtech.com>
2021-05-10 11:51:39 +0530

    analytics

--- resources/views/analytics_list.blade.php
+++ resources/views/analytics_list.blade.php
@@ -162,8 +162,8 @@
                     <div class="card-body">
                     <div class="form-group row">
                          
-                    <div class="col-sm-2 offset-1" style="text-align: center;">
-                                    <label class="col-form-label text-md-right c">Average Daily Rate</label>
+                    <div class="col-sm-2 card" style="text-align: center;margin-left:2%!important">
+                                    <label class="col-form-label text-md-right c" style="text-align: center!important;">Average Daily Rate</label>
 
                                     <p id="dr">{{$performance['daily_rate']}}</p>
 
@@ -171,16 +171,16 @@
                                 </div>
 
 
-                                <div class="col-sm-2" style="text-align: center;">
-                                    <label class="col-form-label text-md-right c">Cancellation Rate</label>
+                                <div class="col-sm-2 card" style="text-align: center;margin-left:35px!important">
+                                    <label class="col-form-label text-md-right c" style="text-align: center!important;">Cancellation Rate</label>
 
                                     <p id="cr">{{$performance['cancel_rate']}}</p>
 
 
                                 </div>
 
-                                <div class="col-sm-2" style="text-align: center;">
-                                    <label class="col-form-label text-md-right c">Revenue</label>
+                                <div class="col-sm-2 card" style="text-align: center;margin-left:35px!important">
+                                    <label class="col-form-label text-md-right c" style="text-align: center!important;">Revenue</label>
 
                                     <p id="rev">£ {{$performance['revenue']}}</p>
 
@@ -188,16 +188,16 @@
                                 </div>
 
 
-                                <div class="col-sm-2" style="text-align: center;">
-                                    <label class="col-form-label text-md-right c">Nights Stayed</label>
+                                <div class="col-sm-2 card" style="text-align: center;margin-left:35px!important">
+                                    <label class="col-form-label text-md-right c" style="text-align: center!important;">Nights Stayed</label>
 
                                     <p id="stay">{{$performance['nights_stayed']}}</p>
 
 
                                 </div>
 
-                                <div class="col-sm-2" style="text-align: center;">
-                                    <label class="col-form-label text-md-right c">Bookings Made</label>
+                                <div class="col-sm-2 card" style="text-align: center;margin-left:35px!important">
+                                    <label class="col-form-label text-md-right c" style="text-align: center!important;">Bookings Made</label>
 
                                     <p id="bm">{{$performance['booking_made']}}</p>
 
